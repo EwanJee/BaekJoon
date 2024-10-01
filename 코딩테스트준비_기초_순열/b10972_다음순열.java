cffba6cd9643b5c834c4fc6e716b2f6f863a98d2
Ewan Jee <830jiforcareer@gmail.com>
2024-10-01 01:45:29 -0400

    코테

--- 코딩테스트준비_기초_순열/b10972_다음순열.java
+++ 코딩테스트준비_기초_순열/b10972_다음순열.java
@@ -8,61 +8,45 @@ public class b10972_다음순열 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
-        int[] a = new int[n];
-
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        if(next_Permutation(a)) {
-            for(int val : a) {
+        if (next_permutation(arr)) {
+            for (int val : arr) {
                 sb.append(val).append(" ");
             }
-        }else {
+        } else {
             sb.append("-1");
         }
-
         System.out.println(sb);
     }
 
-    public static boolean next_Permutation(int[] a) {
-        int i = a.length-1;
-
-        //1. A[i-1] < A[i] 를 만족하는 가장 큰 i를 찾는다.
-        while(i > 0 && a[i-1] >= a[i]) {
+    private static boolean next_permutation(int[] arr) {
+        int i = arr.length - 1;
+        while (i > 0 && arr[i - 1] >= arr[i]) {
             i -= 1;
         }
-
-        //i의 위치가 0이면 내림차순(마지막 순열)
-        if(i<=0) return false;
-
-        int j = a.length - 1;
-
-        //2. j >= i 이면서 A[j] > A[i-1] 을 만족하는 가장 큰 j를 찾는다.
-        while(a[i-1] >= a[j]) {
+        if (i <= 0) return false;
+        int j = arr.length - 1;
+        while (arr[i - 1] >= arr[j]) {
             j -= 1;
         }
+        int temp = -arr[j];
+        arr[j] = arr[i - 1];
+        arr[i - 1] = temp;
 
-        //3. A[i-1]과 A[j] 를 swap 한다.
-        int temp = a[j];
-        a[j] = a[i-1];
-        a[i-1] = temp;
+        j = arr.length - 1;
 
-        j = a.length-1;
-
-
-        //4. A[i] 부터 순열을 뒤집는다.
-        while(i < j) {
-            temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
+        while (i < j) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
             i += 1;
             j -= 1;
         }
-
         return true;
     }
 }
